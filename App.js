cec40074887640a828bfe64f5c5f40030e78a564
lovearya <lovepreetarya1405@gmail.com>
2022-04-27 02:19:59 +0530

    bottom bar

--- App.js
+++ App.js
@@ -1,67 +1,10 @@
-import { View, Text } from "react-native";
-import Home from "./screens/Home";
-import Contests from "./screens/Contests";
-import Placement from "./screens/Placement";
-import Notification from "./screens/Notification";
-import Profile from "./screens/Profile";
-import Search from "./screens/Search";
+import * as React from 'react';
+import MainContainer from './navigation/MainContainer';
 
-import {createMaterialBottomTabNavigator} from '@react-navigation/material-bottom-tabs';
-import {NavigationContainer} from '@react-navigation/native';
-
-import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
-import { colors } from "react-native-elements";
-
-const Tab = createMaterialBottomTabNavigator()
-
-export default function App() {
+function App() {
   return (
-    <NavigationContainer>
-      <Tab.Navigator labeled={false} barStyle={{backgroundcolor:'black'}} activeColor='white'>
-        <Tab.Screen name='Home' component={Home}
-        options={{
-          tabBarIcon:({color,size})=>(
-            <MaterialCommunityIcons name='home' color={color} size={26}/>
-          )
-        }}
-        />
-
-        <Tab.Screen name='Search' component={Search}
-        options={{
-          tabBarIcon:({color,size})=>(
-            <MaterialCommunityIcons name='magnify' color={color} size={26}/>
-          )
-        }}
-        />
-        <Tab.Screen name='Placements' component={Placement}
-        options={{
-          tabBarIcon:({color,size})=>(
-            <MaterialCommunityIcons name='briefcase' color={color} size={26}/>
-          )
-        }}
-        />
-
-        <Tab.Screen name='Contests' component={Contests}
-        options={{
-          tabBarIcon:({color,size})=>(
-            <MaterialCommunityIcons name='book' color={color} size={26}/>
-          )
-        }}
-        />
-        
-        <Tab.Screen name='Profile' component={Profile}
-        options={{
-          tabBarIcon:({color,size})=>(
-            <MaterialCommunityIcons name='account-circle' color={color} size={26}/>
-          )
-        }}
-        />
-
-
-      
-
-      </Tab.Navigator>
-    </NavigationContainer>
-    
+    <MainContainer/>
   );
 }
+
+export default App;
---
+++ navigation/MainContainer.js
@@ -0,0 +1,69 @@
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+
+// Screens
+import Contests from './screens/Contests'
+import HomeScreen from './screens/HomeScreen';
+import Profile from './screens/Profile';
+import Search from './screens/Search';
+import Placement from './screens/Placement';
+
+//Screen names
+const homeName = "Home";
+const ConstProfile = "Profile";
+const ConstSearch = "Search";
+const ConstPlacement = "Placement";
+const ConstContests = "Contests";
+
+const Tab = createBottomTabNavigator();
+
+function MainContainer() {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator
+        initialRouteName={homeName}
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
+            let rn = route.name;
+
+            if (rn === homeName) {
+              iconName = focused ? 'home' : 'home-outline';
+
+            } else if (rn === ConstProfile) {
+              iconName = focused ? 'man' : 'man';
+            
+            } else if (rn === ConstSearch) {
+                iconName = focused ? 'search' : 'search-outline';
+            
+            } else if (rn === ConstPlacement) {
+                iconName = focused ? 'briefcase' : 'briefcase';
+            } else if (rn === ConstContests) {
+                iconName = focused ? 'book' : 'book';
+            }
+
+            // You can return any component that you like here!
+            return <Ionicons name={iconName} size={size} color={color} />;
+          },
+        })}
+        tabBarOptions={{
+          activeTintColor: 'tomato',
+          inactiveTintColor: 'grey',
+          labelStyle: { paddingBottom: 5, fontSize: 10 },
+          style: { padding: 10, height: 70}
+        }}>
+
+        <Tab.Screen name={homeName} component={HomeScreen} />
+        <Tab.Screen name={ConstSearch} component={Search} />
+        <Tab.Screen name={ConstPlacement} component={Placement} />
+        <Tab.Screen name={ConstContests} component={Contests} />
+        <Tab.Screen name={ConstProfile} component={Profile} />
+
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default MainContainer;
